b198afeee81b4b230b5396c62080bcd131ec32ea
S. DHALIA21 <sdhalia.007@gmail.com>
2021-01-26 12:37:54 +0530

    9 update

--- src/db/db.js
+++ src/db/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-mongoose.connect("mongodb+srv://register:<password>@travl.xgnwl.mongodb.net/<dbname>?retryWrites=true&w=majority",{
+mongoose.connect("mongodb+srv://register:dt3CSpy0YBxrFf2y@travl.xgnwl.mongodb.net/<dbname>?retryWrites=true&w=majority",{
     useNewUrlParser:true,
     useUnifiedTopology:true,
     useCreateIndex:true
